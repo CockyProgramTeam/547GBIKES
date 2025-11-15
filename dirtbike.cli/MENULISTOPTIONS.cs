da25023651c9f664032f8e3391df1e3515994924
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2025-11-15 14:16:18 -0500

    Update MENULISTOPTIONS.cs

--- dirtbike.cli/MENULISTOPTIONS.cs
+++ dirtbike.cli/MENULISTOPTIONS.cs
@@ -23,19 +23,6 @@ namespace MENUSYSTEM33
     public class Menu33
     {
 
-        // Declare 3 variables - id, age and name 
-        int id;
-        string storecode;
-        string manager;
-        string state;
-
-        public class Product
-        {
-            public int ProductID { get; set; }
-            public string ProductName { get; set; }
-            public decimal Price { get; set; }
-        }
-
         public override string ToString()
         {
             return "LinqSelected:" + id + " " + manager + "        " + storecode + "," + state;
@@ -46,42 +33,20 @@ namespace MENUSYSTEM33
         {
             T4LISTS OurLists2 = new T4LISTS();
 
-            List<Menu33> stores = new List<Menu33>()
-    { 
-          
-        // Create 5 Student details 
-        new Menu33{ id = 101, manager = "Sravan", storecode = "Forrest33", state = "SC" },
-        new Menu33{ id = 102, manager = "deepu", storecode = "Chicago01" , state = "IL"},
-        new Menu33{ id = 103, manager = "manoja", storecode = "Dallas01" , state= "TX"},
-        new Menu33{ id = 104, manager = "Sathwik", storecode = "Sacramento01" ,  state="CA" },
-        new Menu33{ id = 105, manager = "Saran", storecode = "Seatle01", state = "WA" },
-        new Menu33{ id = 106, manager = "Joe", storecode = "Smithfield01", state = "VA" },
-        new Menu33{ id = 107, manager = "April", storecode = "Stohrs01", state = "VT" }
-    };
-
-            List<Product> products = new List<Product>
-        {
-        new Product { ProductName = "Sr. Ticket", Price = 5.99m },
-        new Product { ProductName = "Child Ticket", Price = 4.99m },
-        new Product { ProductName = "Adult Ticket", Price = 8.99m }
-        };
             int exit = 0;
             int number = 0;
             do
             {
-                Console.WriteLine("\nSystem Utilities: GEM[V6.01] Installation and Maintenance.");
-                Console.WriteLine("Items #1-8 Show Our Default Data To Be Loaded Into the System At Installation Time.");
+                Console.WriteLine("\nSystem Utilities: Parks[V3.01] Installation and Maintenance.");
+                Console.WriteLine("Items #1-5 Show Our Default Data To Be Loaded Into the System At Installation Time.");
                 Console.WriteLine("Please Enter Your Choice:");
-                Console.WriteLine("1.Movies:");
-                Console.WriteLine("2.Genres:");
-                Console.WriteLine("3.Ratings:");
-                Console.WriteLine("4.Showtimes:");
+                Console.WriteLine("1.Parks:");
+                Console.WriteLine("2.Users:");
+                Console.WriteLine("3.CartMasters:");
+                Console.WriteLine("4.Employees:");
                 Console.WriteLine("5.Regions:");
-                Console.WriteLine("6.Stores:");
-                Console.WriteLine("7.Employees:");
-                Console.WriteLine("8.Users:");
                 Console.WriteLine("99.Exit:");
-                Console.WriteLine("Please Enter Your Choice(1,2,3,4,5,6,7,8,99(Exit):\n");
+                Console.WriteLine("Please Enter Your Choice(1,2,3,4,5,99(Exit):\n");
                 string somestring = "1";
                 char myChar = '1';
                 somestring = Console.ReadLine();
@@ -96,31 +61,29 @@ namespace MENUSYSTEM33
                 }
                 else if (number == 1)
                 {
-                    Console.WriteLine("You Choose Option: 1-Movies\n");
-                    Console.WriteLine(String.Join("\n", OurLists2.movies));
+                    Console.WriteLine("You Choose Option: 1-Parks\n");
+                    Console.WriteLine(String.Join("\n", OurLists2.parks));
                     Console.WriteLine("\n\n");
-                    //Directory.SetCurrentDirectory(pString);
-                    //Process.Start("powershell.exe", "cls");
                     exit = 1;
                 }
                 else if (number == 2)
                 {
-                    Console.WriteLine("You Choose Option: 2-Genres\n");
-                    Console.WriteLine(String.Join("\n", OurLists2.genres));
+                    Console.WriteLine("You Choose Option: 2-Users\n");
+                    Console.WriteLine(String.Join("\n", OurLists2.users));
                     Console.WriteLine("\n\n");
                     exit = 2;
                 }
                 else if (number == 3)
                 {
                     Console.WriteLine("You Choose Option: 3-Ratings\n");
-                    Console.WriteLine(String.Join("\n", OurLists2.ratings));
+                    Console.WriteLine(String.Join("\n", OurLists2.cartmasters));
                     Console.WriteLine("\n\n");
                     exit = 3;
                 }
                 else if (number == 4)
                 {
                     Console.WriteLine("You Choose Option: 4-Showtimes\n");
-                    Console.WriteLine(String.Join("\n", OurLists2.showtimes));
+                    Console.WriteLine(String.Join("\n", OurLists2.employees));
                     Console.WriteLine("\n\n");
                     exit = 4;
                 }
@@ -131,38 +94,7 @@ namespace MENUSYSTEM33
                     Console.WriteLine("\n\n");
                     exit = 5;
                 }
-                else if (number == 6)
-                {
-                    Console.WriteLine("You Choose Option: 6-Stores\n");
-                    // Iterate the Stores by selecting stores // name starts with S  // Using Where() function 
-                    IEnumerable<Menu33> Query = stores.Where(s => s.storecode[0] == 'S');
-
-                    // Display employee details 
-                    Console.WriteLine("LinqSelected:ID  Manager              Storecode State");
-                    Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++");
-                    foreach (Menu33 e in Query)
-                    {
-
-                        // Call the to string method 
-                        Console.WriteLine(e.ToString());
-                    }
-                    Console.WriteLine("\n\n");
-                    exit = 6;
-                }
-                else if (number == 7)
-                {
-                    Console.WriteLine("You Choose Option: 7-Employees\n");
-                    Console.WriteLine(String.Join("\n", OurLists2.employees));
-                    Console.WriteLine("\n\n");
-                    exit = 7;
-                }
-                else if (number == 8)
-                {
-                    Console.WriteLine("You Choose Option: 8-Users\n");
-                    Console.WriteLine(String.Join("\n", OurLists2.users));
-                    Console.WriteLine("\n");
-                    exit = 8;
-                }
+               
                 else
                 {
                     Console.WriteLine("\nYou Have Selected to Leave. Thank You.");
